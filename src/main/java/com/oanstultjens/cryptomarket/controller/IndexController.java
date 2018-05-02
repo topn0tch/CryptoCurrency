15ef66d972087fe6f5d7965bdfc6bb454cb31a76
Oan Stultjens <oan.stultjens@hotmail.com>
2018-05-02 07:49:20 +0200

    - Added images per currency, and a default if the image doesn't exist for a specific currency.

--- src/main/java/com/oanstultjens/cryptomarket/controller/IndexController.java
+++ src/main/java/com/oanstultjens/cryptomarket/controller/IndexController.java
@@ -1,7 +1,24 @@
 package com.oanstultjens.cryptomarket.controller;
 
+import com.oanstultjens.cryptomarket.model.CryptoCurrency;
+import com.oanstultjens.cryptomarket.service.CryptoCurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    CryptoCurrencyService cryptoCurrencyService;
+
+    @GetMapping("/")
+    public String getIndex(Model model) {
+        List<CryptoCurrency> cryptoCurrencyList = cryptoCurrencyService.findAll();
+        model.addAttribute("allCryptoCurrencies", cryptoCurrencyList);
+        return "index";
+    }
 }
